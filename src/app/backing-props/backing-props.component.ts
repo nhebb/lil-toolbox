9ea0ac520dee7baeb71af1896c43f584700627aa
Nicholas Hebb <nhebb@breezetree.com>
2020-08-15 21:31:39 -0700

    Added threadsafe option for backing props setter

--- src/app/backing-props/backing-props.component.ts
+++ src/app/backing-props/backing-props.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BackingPropsService  } from './backing-props.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-backing-props',
   templateUrl: './backing-props.component.html',
   styleUrls: ['./backing-props.component.css']
 })
 export class BackingPropsComponent implements OnInit {
+  propsForm = new FormGroup({
+    inputText: new FormControl('', [Validators.required]),
+    outputText: new FormControl(''),
+    threadsafe: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(private backingPropsService: BackingPropsService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(form: FormGroup): void {
+    const input =  form.value.inputText;
+    const threadsafe = form.value.threadsafe;
+
+    const result = this.backingPropsService.createBackingProps(input, threadsafe)
+
+    form.patchValue({outputText: result});
   }
 
 }
