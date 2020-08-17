87a62969d8452d0a747c98de4ec52826302969bd
Nicholas Hebb <nhebb@breezetree.com>
2020-08-16 21:31:35 -0700

    Added implmentation of Swap Assignment. Modified the user-facing descriptions in several HTML files.

--- src/app/swap-assignment/swap-assignment.component.ts
+++ src/app/swap-assignment/swap-assignment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-swap-assignment',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwapAssignmentComponent implements OnInit {
 
+  propsForm = new FormGroup({
+    inputText: new FormControl('', [Validators.required]),
+    outputText: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: FormGroup): void {
+    const input =  form.value.inputText;
+    const result = this.swapAssignment(input);
+    form.setValue({outputText: result});
+  }
+
+  private swapAssignment(input: string): string {
+    const output: string[] = [];
+    const lines = input.replace('\r\n', '\n').split('\n');
+    for (const line of lines) {
+      const txt = line.replace(';', '');
+      const parts = txt.split('=');
+
+      if (parts.length !== 2) { continue; }
+
+      output.push(`${parts[1].trim()} = ${parts[0].trim()};`);
+    }
+    return output.join('\r\n');
+  }
 }
